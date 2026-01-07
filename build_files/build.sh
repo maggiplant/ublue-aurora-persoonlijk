f28363324a77d8c2eb9cd056b75df111c9b6a719
Joeke <joeke@posteo.net>
2026-01-07 19:46:07 +0100

    Eerste versie van het build-script voor het installeren van Emacs

--- build_files/build.sh
+++ build_files/build.sh
@@ -9,8 +9,27 @@ set -ouex pipefail
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+# Install build dependencies for Emacs
+sudo dnf5 builddep emacs-pgtk
+sudo dnf5 install ImageMagick-devel
+
+# Get the latest Emacs tar
+curl -o emacs.tar.xz -L https://ftpmirror.gnu.org/gnu/emacs/emacs-30.2.tar.xz
+tar xJf emacs.tar.xz --one-top-level=emacs --strip-components=1
+rm emacs.tar.xz
+cd emacs
+
+# Configure, build and install
+./configure -C --with-native-compilation=aot --disable-gc-mark-trace --with-imagemagick --with-mailutils --with-tree-sitter --with-pgtk
+sudo make install
+
+# Remove the code
+cd ..
+rm -r ./emacs
+
+# Remove build dependecies
+LANG=C sudo dnf5 builddep emacs 2>&1 | awk -F'"' '/^Package .* is already installed/ {print $2}' | xargs sudo dnf5 remove
+sudo dnf5 remove ImageMagick-devel
 
 # Use a COPR Example:
 #
@@ -21,4 +40,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
